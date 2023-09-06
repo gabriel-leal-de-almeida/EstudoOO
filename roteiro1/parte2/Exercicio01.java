dc17a45afd2eddb964d8cb04f4b109f6ab7ab59e
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-05 21:38:57 -0300

    feat: adiciona variáveis de entrada e validações

--- roteiro1/parte2/Exercicio01.java
+++ roteiro1/parte2/Exercicio01.java
@@ -1,15 +1,38 @@
 package roteiro1.parte2;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
-        double salarioBase = 2500; // R$
-        int numHoraExtra = 10; // horas
-        double valorHora = salarioBase / 200; // 200 = 40 horas semanais * 5 semanas
-        double valorHoraExtra = numHoraExtra * valorHora; // R$/hora
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite o salário base: ");
+            double salarioBase = scanner.nextDouble();
+            if (salarioBase < 0) {
+                System.out.println("Salário base não pode ser negativo.");
+                return;
+            }
 
-        System.out.println("Salario base: " + salarioBase);
-        System.out.println("Valor hora: " + valorHora);
-        System.out.println("Valor hora extra: " + valorHoraExtra);
-        System.out.println("Salario final: " + (salarioBase + valorHoraExtra));
-}
+            System.out.print("Digite o número de horas extras: ");
+            int numHoraExtra = scanner.nextInt();
+            if (numHoraExtra < 0) {
+                System.out.println("Número de horas extras não pode ser negativo.");
+                return;
+            }
+
+            System.out.print("Digite o número de horas trabalhadas por mês: ");
+            int numHorasTrabalhadas = scanner.nextInt();
+            if (numHorasTrabalhadas < 0) {
+                System.out.println("Número de horas trabalhadas não pode ser negativo.");
+                return;
+            }
+
+            double valorHora = salarioBase / numHorasTrabalhadas;
+            double valorHoraExtra = numHoraExtra * valorHora;
+
+            System.out.println("Salario base: " + salarioBase);
+            System.out.println("Valor hora: " + valorHora);
+            System.out.println("Valor hora extra: " + valorHoraExtra);
+            System.out.println("Salario final: " + (salarioBase + valorHoraExtra));
+        }
+    }
 }
