abf3d0068901f5d87f301df1584c069b1cf68294
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-07 17:00:03 -0300

    feat: add class Programa05

---
+++ roteiro2/parte3/Programa05.java
@@ -0,0 +1,31 @@
+/*
+* 2 – Crie a classe Programa05 no mesmo pacote de forma semelhante ao Programa04.
+* Este novo programa deve apresentar o mesmo resultado anterior, mas usando o for como estrutura de repetição
+*/
+
+package roteiro2.parte3;
+
+import java.util.Scanner;
+
+public class Programa05 {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        for (int cont = 0; cont < 5; cont++) {
+            System.out.println("Informe uma nota: ");
+            int nota = entrada.nextInt();
+
+            if (nota < 0 || nota > 10) {
+                System.out.println("Nota inválida!");
+                continue;
+            }
+
+            if (nota >= 7) {
+                System.out.println("Aprovado!");
+            } else {
+                System.out.println("Reprovado!");
+            }
+        }
+    }
+}
