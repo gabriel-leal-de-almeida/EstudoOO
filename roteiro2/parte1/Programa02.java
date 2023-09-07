d0ea483711d99b7003db2e9d4e3734fc714b217e
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-07 16:43:22 -0300

    docs: add docstrings to class Programa02

--- roteiro2/parte1/Programa02.java
+++ roteiro2/parte1/Programa02.java
@@ -1,3 +1,11 @@
+/*
+* 2 – Crie agora uma classe no mesmo pacote chamado Programa02.
+* Neste programa vamos capturar a entrada de dados do usuário, e para isso precisaremos importar a classe Scanner do Java.
+* Implemente a classe Programa02 conforme o código abaixo
+* 3 – Faça os devidos testes rodando o Programa02 e teste como seria uma entrada de dados de um tipo numérico.
+* OBS.: a classe Scanner faz uso de conceitos de OO que serão melhor detalhados posteriormente.
+*/
+
 package roteiro2.parte1;
 
 import java.util.Scanner;
