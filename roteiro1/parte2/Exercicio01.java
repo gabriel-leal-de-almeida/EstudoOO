4020b706e0813e6bd0f374f8e3fd42ede7b5c855
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-07 16:35:57 -0300

    docs: add docstrings to class Exercicio01

--- roteiro1/parte2/Exercicio01.java
+++ roteiro1/parte2/Exercicio01.java
@@ -1,3 +1,9 @@
+// Mantendo o padrão de organização dos nossos estudos, crie um pacote dentro do projeto e coloque o nome roteiro1.parte2.
+// Cálculo de Salário.
+// Escreva um programa em Java para calcular o salário total de um funcionário. O programa deve conter o valor do salário-base mensal que compreende um total de 40 horas. O programa também deve conter o número de horas extra trabalhadas no mês.
+// Em seguida, calcule o salário total considerando que cada hora extra trabalhada vale o mesmo que uma hora regular. Por fim, exiba o salário total na tela.
+// O programa abaixo já tem o escopo do programa imprimindo o salário base. Complemente o programa para atender o que se pede informando o salário total. Faça também os devidos testes e simulações para as horas extra.
+// Dica: O valor da hora extra é calculado dividindo o salário-base pelo número de horas trabalhadas no mês.
 package roteiro1.parte2;
 
 import java.util.Scanner;
