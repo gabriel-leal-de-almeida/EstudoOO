5361c0128fd18daaf881b9e9f28f54c2d43a1c3e
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-07 15:23:01 -0300

    feat: class Programa02

---
+++ roteiro2/parte1/Programa02.java
@@ -0,0 +1,16 @@
+package roteiro2.parte1;
+
+import java.util.Scanner;
+
+public class Programa02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe o seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.print("Nome informado: " + nome);
+
+        entrada.close();
+}
+}
