96da5b9f1af45f12fcdc1c1f850b4ee9dd675b56
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-19 20:40:11 -0300

    feat: add roteiro5.parte2 class Principal

---
+++ roteiro5/parte2/Principal.java
@@ -0,0 +1,84 @@
+/*
+
+1 – Crie o pacote roteiro5.parte2 com a cópia das classes Principal e Loja implementadas na parte1.
+
+2 -  Iremos agora aplicar os nossos conhecimentos de Sobrecarga de Métodos. A solução adotada no item 5 da Parte1 apesar de funcionar não é muito adequada. Podemos utilizar recursos da OO para tornar o código mais limpo e até reutilizável.
+
+O adequado neste caso é aplicar a sobrecarga no método construtor. Ou seja, teremos 2 construtores que recebem diferentes parâmetros, cada um deve ser utilizado conforme a necessidade. 
+
+2 – Aplique agora as chamadas adequadas na classe Principal.
+
+3 – Com sobrecarga do método construtor podemos otimizar o código ainda mais promovendo reutilização de código.
+
+Podemos apenas chamar o 1º Construtor de dentro do 2º Construtor passando os parâmetros da forma desejada. Assim eliminamos linhas de código desnecessárias, pois estaremos de fato fazendo reutilização de código.
+
+4 – Para atividade final faça os testes abaixo na classe Principal. O objetivo é compreender como funciona os objetos instanciados e como testá-los.
+
+Abaixo fizemos 4 testes (Teste1, Teste2, Teste3, Teste4, Teste5 ). Avalie os resultados apresentados e a diferença entre eles. 
+
+O que conseguiu concluir sobre os testes feitos ?
+R: Os testes 1 e 2 têm como output o mesmo resultado, pois o operador == compara o endereço de memória dos objetos, e o operador .equals compara o conteúdo dos objetos. Nas duas situações, a condição era verdadeira, o que resultou em "Lojas Iguais". Já nos testes 3 e 4, o output foi diferente, pois o operador == compara o endereço de memória dos objetos, e o operador .equals compara o conteúdo dos objetos. Nas duas situações, a condição era falsa, o que resultou em "Lojas Diferentes". No teste 5, o output foi "Faturamentos Iguais", pois o operador == compara o endereço de memória dos objetos. Nessa situação, a condição era verdadeira.
+
+No Teste5 é possível utilizar o operador .equals ? 
+R: Não é possível utilizar o operador .equals para comparar tipos primittivos, apenas para objetos.
+
+*/
+
+
+package roteiro5.parte2;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+		Loja loja01 = new Loja("Lojão da Cidade", "Lojão Comércio LTDA", "11223344");
+		Loja loja02 = new Loja("Mercadão do Povo", "", "10101010");
+		Loja loja03 = new Loja("Lojão da Cidade", "Lojão Comércio LTDA", "11223344");
+
+		loja01.setValorFat(10000);
+		loja02.setValorFat(20000);
+		loja03.setValorFat(10000);
+
+		System.out.println(" ****** Teste 1 ********");
+		if ( loja01.getNomeFantasia() == loja03.getNomeFantasia() ){
+			System.out.println("Lojas Iguais");
+		}
+		else {
+			System.out.println("Lojas Diferentes");
+		}
+
+		System.out.println(" ****** Teste 2 ********");
+		if ( loja01.getNomeFantasia().equals(loja03.getNomeFantasia())  ){
+			System.out.println("Lojas Iguais");
+		}
+		else {
+			System.out.println("Lojas Diferentes");
+		}
+			
+		System.out.println(" ****** Teste 3 ********");
+		if ( loja01 == loja03  ){
+			System.out.println("Lojas Iguais");
+		}
+		else {
+			System.out.println("Lojas Diferentes");
+		}
+		
+		System.out.println(" ****** Teste 4 ********");
+		if ( loja01.equals(loja03)  ){
+			System.out.println("Lojas Iguais");
+		}
+		else {
+			System.out.println("Lojas Diferentes");
+		}
+
+		System.out.println(" ****** Teste 5 ********");
+		if ( loja01.getValorFat() == loja03.getValorFat()  ){
+			System.out.println("Faturamentos Iguais");
+		}
+		else {
+			System.out.println("Faturamentos Diferentes");
+		}
+		
+			
+	}  
+}
