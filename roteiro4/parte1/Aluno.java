f9731b61eabe8cfdb38127a4cc87de7662acce42
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-09-19 19:13:23 -0300

    docs: add header to roteiro4.parte1 class Aluno

--- roteiro4/parte1/Aluno.java
+++ roteiro4/parte1/Aluno.java
@@ -1,3 +1,15 @@
+/*
+1 – Crie o pacote roteiro4.parte1 com a classes Principal  e Aluno conforme o código abaixo. 
+
+Atenção : 
+•	A boa prática da programação OO indica que os nomes das classes devem sempre iniciar com letras maiúsculas.
+•	A classe Aluno tem apenas os atributos : matrícula, nome, curso, anoIngresso. 
+•	E a classe Principal tem o método main que utilizamos ele para instanciar o objeto aluno01, conforme o destaque em amarelo.
+
+
+Faça os testes rodando a classe Principal.
+*/
+
 package roteiro4.parte1;
 
 public class Aluno {
