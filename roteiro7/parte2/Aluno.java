6e84dde2274c605f71eb2f1adea38a23d0225838
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-10-06 18:56:21 -0300

    feat: add roteiro7.parte2 class Aluno

---
+++ roteiro7/parte2/Aluno.java
@@ -0,0 +1,70 @@
+package roteiro7.parte2;
+
+public class Aluno {
+
+    private int matricula;
+    private String nome;
+    private String curso;
+    private int anoIngresso;
+    private int qtdeDisciplinas;
+    private String situacao;
+    
+    Aluno(int pMatricula, String pNome, String pCurso, int pAnoIngresso, int pQtdeDisciplinas){
+        matricula = pMatricula;
+        nome = pNome;
+        curso = pCurso;
+        anoIngresso = pAnoIngresso;
+        qtdeDisciplinas = pQtdeDisciplinas;     
+    }
+    
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    
+    public void setCurso() {
+        this.curso = curso;
+    }
+
+
+    public int getAnoIngresso() {
+        return anoIngresso;
+    }
+    
+    public void setAnoIngresso() {
+        this.anoIngresso = anoIngresso;
+    }
+    
+    public int getQtdeDisciplinas() {
+        return qtdeDisciplinas;
+    }
+    
+    public void setQtdeDisciplinas() {
+        this.qtdeDisciplinas = qtdeDisciplinas;
+    }
+        
+    public String getSituacao() {
+        return situacao;   
+    }
+    
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+}
+
+
