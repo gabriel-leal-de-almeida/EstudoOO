5437bd4e85b703bd17048c6cf40fa842a457a22b
gabriel-leal-de-almeida <112120035+gabriel-leal-de-almeida@users.noreply.github.com>
2023-10-06 18:57:53 -0300

    feat: add roteiro8.parte1 class Principal

---
+++ roteiro8/parte1/Principal.java
@@ -0,0 +1,11 @@
+package roteiro8.parte1;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+    VeiculoCarga v2 = new VeiculoCarga("22bb", 2000, 1000);
+    VeiculoPasseio v3 = new VeiculoPasseio("333ccc", 2020,7);
+    VeiculoPequeno v4 = new VeiculoPequeno("PKW-1J77", 2018,"Up");
+    }
+}
